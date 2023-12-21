57a60f894bab144b5f2b560530b67bb20ff07d47
Cody Hardy <chardy0919@gmail.com>
2023-12-21 12:27:12 -0500

    seraializer moves and update to pokemon serializer

--- back-end/pokemon_app/serializers.py
+++ back-end/pokemon_app/serializers.py
@@ -4,6 +4,17 @@ from .models import Pokemon # import Pokemon model from models.py
 
 class PokemonSerializer(serializers.ModelSerializer):
 
+    # class Meta:
+    #     model = Pokemon # specify what model this serializer is for
+    #     fields = ['id', 'name', 'level'] # specify the fields you would like this serializer to return
+
+    moves = serializers.SerializerMethodField()
+
     class Meta:
         model = Pokemon # specify what model this serializer is for
-        fields = ['id', 'name', 'level'] # specify the fields you would like this serializer to return
+        fields = ['id', 'name', 'level', 'moves'] # specify the fields you would like this serializer to return
+
+    def get_moves(self, instance):
+        moves = instance.moves.all()
+        move_names = [move.name for move in moves]
+        return move_names
